be1f69e1fb8163ea68794af1bfc27fa52fa66510
acktarius <xchaps@aol.com>
2024-10-26 10:42:05 -0400

    headers

--- ping_ccx_pool.cpp
+++ ping_ccx_pool.cpp
@@ -1,3 +1,4 @@
+#include"mainFrame.hpp"
 #include "ping_ccx_pool.hpp"
 #include <cstdlib>
 #include <iostream>
@@ -20,90 +21,7 @@
 
 using json = nlohmann::json;
 
-// Global variables implementation
-std::map<int, std::vector<std::string>> poolAndPort;
 
-// MainFrame implementation
-MainFrame::MainFrame(const wxString& title, const wxString& gitVersion)
-    : wxFrame(nullptr, wxID_ANY, title), m_gitVersion(gitVersion) {
-    InitializePoolData();
-    
-    // Create a panel to hold our controls
-    wxPanel* panel = new wxPanel(this);
-    
-    // Create a sizer for the panel
-    wxBoxSizer* mainSizer = new wxBoxSizer(wxVERTICAL);
-
-    // Create a horizontal sizer for the banner and image
-    wxBoxSizer* bannerSizer = new wxBoxSizer(wxHORIZONTAL);
-
-    // Add a spacer on the left to push the text to the center
-    bannerSizer->Add(0, 0, 1, wxEXPAND);
-
-    // Add the banner text
-    wxStaticText* bannerText = new wxStaticText(panel, wxID_ANY, "Ping CCX Pool");
-    wxFont bannerFont = bannerText->GetFont();
-    bannerFont.SetPointSize(bannerFont.GetPointSize() + 16 );
-    bannerFont.SetWeight(wxFONTWEIGHT_BOLD);
-    bannerText->SetFont(bannerFont);
-    bannerText->SetForegroundColour(wxColour(255, 125, 0));  // Set to red, adjust as needed
-    bannerSizer->Add(bannerText, 0, wxALIGN_CENTER_VERTICAL);
-
-    // Add a spacer on the right to balance the layout
-    bannerSizer->Add(0, 0, 1, wxEXPAND);
-
-    // Load and add the image
-    wxImage image;
-    if (image.LoadFile("pp.png", wxBITMAP_TYPE_PNG))
-    {
-        // Scale the image to desired size (e.g., 32x32)
-        image.Rescale(64, 64, wxIMAGE_QUALITY_HIGH);
-        wxBitmap bitmap(image);
-        m_logoImage = new wxStaticBitmap(panel, wxID_ANY, bitmap);
-        bannerSizer->Add(m_logoImage, 0, wxALIGN_CENTER_VERTICAL | wxLEFT, 10);
-    }
-    //bannerSizer->Add(0, 0, 0.2, wxEXPAND);
-
-    // Add the banner sizer to the main sizer
-    mainSizer->Add(bannerSizer, 0, wxEXPAND | wxALL, 10);
-
-    // Add the version information
-    wxBoxSizer* versionSizer = new wxBoxSizer(wxHORIZONTAL);
-    wxString shortVersion = wxString(m_gitVersion).BeforeFirst('-');
-    wxStaticText* versionText = new wxStaticText(panel, wxID_ANY, wxString::Format("Version: %s", shortVersion));
-    versionSizer->Add(0, 0, 1, wxEXPAND); // Add a spacer that can expand
-    versionSizer->Add(versionText, 0, wxALIGN_CENTER_VERTICAL | wxRIGHT, 5);
-    mainSizer->Add(versionSizer, 0, wxEXPAND | wxBOTTOM, 5);
-
-    // Pool selection list
-    wxArrayString poolChoices;
-    for (const auto& pool : poolAndPort) {
-        wxString poolAddress = pool.second[0];
-        poolChoices.Add(poolAddress);
-    }
-    poolListBox = new wxCheckListBox(panel, wxID_ANY, wxDefaultPosition, wxSize(900, 200), poolChoices);
-    mainSizer->Add(poolListBox, 0, wxALIGN_CENTER | wxALL, 10);
-
-    // Start button
-    wxButton* startButton = new wxButton(panel, wxID_ANY, "Start Test");
-    mainSizer->Add(startButton, 0, wxALIGN_CENTER | wxALL, 10);
-
-    // Result text control
-    resultTextCtrl = new wxTextCtrl(panel, wxID_ANY, "", wxDefaultPosition, wxSize(900, 300), wxTE_MULTILINE | wxTE_READONLY);
-    mainSizer->Add(resultTextCtrl, 1, wxEXPAND | wxALL, 10);
-
-    panel->SetSizer(mainSizer);
-    mainSizer->Fit(this);
-
-    wxBoxSizer* frameSizer = new wxBoxSizer(wxVERTICAL);
-    frameSizer->Add(panel, 1, wxEXPAND);
-    this->SetSizer(frameSizer);
-
-    this->SetMinSize(wxSize(1000, 800));
-    this->Fit();
-
-    BindEvents();
-}
 
 void MainFrame::InitializePoolData() {
     const wxString jsonFilePath = "pools.json";
--- ping_ccx_pool.hpp
+++ ping_ccx_pool.hpp
@@ -11,42 +11,6 @@
 #include <wx/bitmap.h>
 #include <wx/statbmp.h>
 
-// Forward declarations
-class MainFrame;
-
-// Global variables (consider making these member variables in a class later)
-extern std::map<int, std::vector<std::string>> poolAndPort;
-
-// Add the PoolResult struct
-struct PoolResult {
-    std::string address;
-    std::string port;
-    double avgRtt;
-
-    PoolResult(std::string addr, std::string p, double rtt)
-        : address(std::move(addr)), port(std::move(p)), avgRtt(rtt) {}
-};
-
-// MainFrame class
-class MainFrame : public wxFrame {
-public:
-    // Update the constructor declaration to include GIT_VERSION
-    MainFrame(const wxString& title, const wxString& gitVersion);
-    void CreateControls();
-
-private:
-    void InitializePoolData();
-    void BindEvents();
-    void OnStartTest(wxCommandEvent& event);
-    PoolResult PerformNpingTest(const std::vector<std::string>& poolInfo);  // Updated return type
-    void SummarizeResults();  // Add this line
-
-    wxTextCtrl* resultTextCtrl;
-    wxCheckListBox* poolListBox;
-    std::vector<PoolResult> poolResults;  // Add this line
-    wxString m_gitVersion;  // Add this line to store the git version
-    wxStaticBitmap* m_logoImage;  // Add this line to store the logo image
-};
 
 // wxApp-derived class
 class MyApp : public wxApp {
